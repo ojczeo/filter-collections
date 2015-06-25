80fb5bc7e39f10cbe40043a03d0b7f2b064aed1f
Kuba Wyrobek <kuba.wyrobek@gmail.com>
2015-06-26 00:41:05 +0200

    code reformat

--- filter-collections-server.js
+++ filter-collections-server.js
@@ -1,95 +1,95 @@
 Meteor.FilterCollections = {};
-var logger = loggerFactory('filter-collections-server')
-Meteor.FilterCollections.publish = function (collection, options) {
+var logger = loggerFactory( 'filter-collections-server' )
+Meteor.FilterCollections.publish = function ( collection, options ) {
 
-  var self = this;
-
-  options = options || {};
-
-  var callbacks = options.callbacks || {};
-
-  // var cursor = {};
-
-  var name = (options.name) ? options.name : collection._name;
-
-  var publisherResultsId = 'fc-' + name + '-results';
-  var publisherCountId = 'fc-' + name + '-count';
-  var publisherCountCollectionName = name + 'CountFC';
-
-  /**
-   * Publish query results.
-   */
-
-  Meteor.publish(publisherResultsId, function (query) {
-
-    check(query, Object);
-
-    var allow = true;
-
-    if (callbacks.allow && _.isFunction(callbacks.allow))
-      allow = callbacks.allow(query, this);
-
-    if(!allow){
-      throw new Meteor.Error(417, 'Not allowed');
-    }
-
-    query = (query && !_.isEmpty(query)) ? query : {};
-
-    query.selector = query.selector || {};
-
-    query.options = query.options || {
-      sort: [],
-      skip: 0,
-      limit: 10
-    };
-
-    if (callbacks.beforePublish && _.isFunction(callbacks.beforePublish))
-      query = callbacks.beforePublish(query, this) || query;
-
-    //logger.log('query.selector\n', JSON.stringify(query.selector, null, 2) )
-    //logger.log('query.options\n', JSON.stringify(query.options, null, 2) )
-
-    var cursor = collection.find(query.selector, query.options);
-
-    if (callbacks.afterPublish && _.isFunction(callbacks.afterPublish))
-      cursor = callbacks.afterPublish('results', cursor, this) || cursor;
-
-    return cursor;
-  });
-
-  /**
-   * Publish result count.
-   */
-
-  Meteor.publish(publisherCountId, function (query) {
-    check(query, Object);
     var self = this;
-    var allow = true;
-    var cursor = {};
 
-    if (callbacks.allow && _.isFunction(callbacks.allow))
-      allow = callbacks.allow(query, this);
+    options = options || {};
 
-    if(!allow){
-      throw new Meteor.Error(417, 'Not allowed');
-    }
+    var callbacks = options.callbacks || {};
 
-    query = (query && !_.isEmpty(query)) ? query : {};
-    query.selector = query.selector || {};
+    // var cursor = {};
 
-    if(callbacks.beforePublish && _.isFunction(callbacks.beforePublish))
-      query = callbacks.beforePublish(query, this) || query;
+    var name = (options.name) ? options.name : collection._name;
 
-    count = collection.find(query.selector).count() || 0;
+    var publisherResultsId = 'fc-' + name + '-results';
+    var publisherCountId = 'fc-' + name + '-count';
+    var publisherCountCollectionName = name + 'CountFC';
 
-    if(callbacks.afterPublish && _.isFunction(callbacks.afterPublish))
-      cursor = callbacks.afterPublish('count', cursor, this) || cursor;
+    /**
+     * Publish query results.
+     */
 
-    self.added(publisherCountCollectionName, Meteor.uuid(), {
-      count: count,
-      query: query
-    });
+    Meteor.publish( publisherResultsId, function ( query ) {
 
-    this.ready();
-  });
+        check( query, Object );
+
+        var allow = true;
+
+        if ( callbacks.allow && _.isFunction( callbacks.allow ) )
+            allow = callbacks.allow( query, this );
+
+        if ( !allow ) {
+            throw new Meteor.Error( 417, 'Not allowed' );
+        }
+
+        query = (query && !_.isEmpty( query )) ? query : {};
+
+        query.selector = query.selector || {};
+
+        query.options = query.options || {
+            sort: [],
+            skip: 0,
+            limit: 10
+        };
+
+        if ( callbacks.beforePublish && _.isFunction( callbacks.beforePublish ) )
+            query = callbacks.beforePublish( query, this ) || query;
+
+        //logger.log('query.selector\n', JSON.stringify(query.selector, null, 2) )
+        //logger.log('query.options\n', JSON.stringify(query.options, null, 2) )
+
+        var cursor = collection.find( query.selector, query.options );
+
+        if ( callbacks.afterPublish && _.isFunction( callbacks.afterPublish ) )
+            cursor = callbacks.afterPublish( 'results', cursor, this ) || cursor;
+
+        return cursor;
+    } );
+
+    /**
+     * Publish result count.
+     */
+
+    Meteor.publish( publisherCountId, function ( query ) {
+        check( query, Object );
+        var self = this;
+        var allow = true;
+        var cursor = {};
+
+        if ( callbacks.allow && _.isFunction( callbacks.allow ) )
+            allow = callbacks.allow( query, this );
+
+        if ( !allow ) {
+            throw new Meteor.Error( 417, 'Not allowed' );
+        }
+
+        query = (query && !_.isEmpty( query )) ? query : {};
+        query.selector = query.selector || {};
+
+        if ( callbacks.beforePublish && _.isFunction( callbacks.beforePublish ) )
+            query = callbacks.beforePublish( query, this ) || query;
+
+        count = collection.find( query.selector ).count() || 0;
+
+        if ( callbacks.afterPublish && _.isFunction( callbacks.afterPublish ) )
+            cursor = callbacks.afterPublish( 'count', cursor, this ) || cursor;
+
+        self.added( publisherCountCollectionName, Meteor.uuid(), {
+            count: count,
+            query: query
+        } );
+
+        this.ready();
+    } );
 };
--- package.js
+++ package.js
@@ -1,19 +1,19 @@
-Package.describe({
-  summary: " Smart package for Meteor that adds filter and pager behavior to our Meteor's collections.",
-  version: "0.2.0",
-  name:'parhelium:filter-collections',
-  git: "https://github.com/parhelium/filter-collections"
-});
+Package.describe( {
+    summary: " Smart package for Meteor that adds filter and pager behavior to our Meteor's collections.",
+    version: "0.2.0",
+    name: 'parhelium:filter-collections',
+    git: "https://github.com/parhelium/filter-collections"
+} );
 
-Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.2.2');
+Package.onUse( function ( api ) {
+    api.versionsFrom( 'METEOR@0.9.2.2' );
 
-  var all = ['client', 'server'];
-  api.use([
-    'underscore',
-    'parhelium:logger'
-  ], all);
+    var all = ['client', 'server'];
+    api.use( [
+        'underscore',
+        'parhelium:logger'
+    ], all );
 
-  api.addFiles('filter-collections-client.js', ['client']);
-  api.addFiles('filter-collections-server.js', ['server']);
-});
+    api.addFiles( 'filter-collections-client.js', ['client'] );
+    api.addFiles( 'filter-collections-server.js', ['server'] );
+} );
