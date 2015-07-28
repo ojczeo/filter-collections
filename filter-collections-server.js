4cd0ca77247238a57efd9b671b148d025a539718
Kuba Wyrobek <kuba.wyrobek@gmail.com>
2015-07-28 08:56:50 +0200

    sync with workpop : filter-collections

--- filter-collections-server.js
+++ filter-collections-server.js
@@ -1,95 +1,145 @@
-Meteor.FilterCollections = {};
-var logger = loggerFactory( 'filter-collections-server' )
-Meteor.FilterCollections.publish = function ( collection, options ) {
+FilterCollections = {};
 
-    var self = this;
+FilterCollections._extendedPublishCursor = function (cursor, sub, collection, name) {
+    var observeHandle = cursor.observeChanges({
+        added: function (id, fields) {
+            // Add the name of this filter collection
+            if (name) {
+                fields.__filter = name;
+            }
+            sub.added(collection, id, fields);
+        },
+        changed: function (id, fields) {
+            if (name) {
+                fields.__filter = name;
+            }
+            sub.changed(collection, id, fields);
+        },
+        removed: function (id) {
+            sub.removed(collection, id);
+        }
+    });
 
+    // We don't call sub.ready() here: it gets called in livedata_server, after
+    // possibly calling _publishCursor on multiple returned cursors.
+
+    // register stop callback (expects lambda w/ no args).
+    sub.onStop(function () {
+        observeHandle.stop();
+    });
+};
+
+FilterCollections.publish = function (collection, options) {
+    var optionalFunction = Match.Optional(Function);
+
+    check(collection, Mongo.Collection);
+    var optionalString = Match.Optional(String);
+    check(options, Match.Optional({
+        name: optionalString,
+        callbacks: Match.Optional({
+            allow: optionalFunction,
+            beforePublish: optionalFunction,
+            afterPublish: optionalFunction
+        })
+    }));
     options = options || {};
-
     var callbacks = options.callbacks || {};
 
-    // var cursor = {};
+    _.defaults(options, {
+        name: collection._name
+    });
 
-    var name = (options.name) ? options.name : collection._name;
-
-    var publisherResultsId = 'fc-' + name + '-results';
-    var publisherCountId = 'fc-' + name + '-count';
-    var publisherCountCollectionName = name + 'CountFC';
+    var publisherResultsCollectionName = options.name;
+    var publisherResultsId = 'fc-' + options.name + '-results';
+    var publisherCountId = 'fc-' + options.name + '-count';
+    var publisherCountCollectionName = options.name + 'CountFC';
 
     /**
      * Publish query results.
      */
 
-    Meteor.publish( publisherResultsId, function ( query ) {
-
-        check( query, Object );
-
+    Meteor.publish(publisherResultsId, function (query) {
+        var self = this;
         var allow = true;
 
-        if ( callbacks.allow && _.isFunction( callbacks.allow ) )
-            allow = callbacks.allow( query, this );
-
-        if ( !allow ) {
-            throw new Meteor.Error( 417, 'Not allowed' );
+        // Check if this publish is allowed.
+        if (callbacks.allow) {
+            allow = callbacks.allow(query, this);
         }
 
-        query = (query && !_.isEmpty( query )) ? query : {};
+        if (!allow) {
+            throw new Meteor.Error(417, 'Not allowed');
+        }
 
-        query.selector = query.selector || {};
+        query = query || {};
 
-        query.options = query.options || {
+        _.defaults(query, {
+            selector: {},
+            options: {}
+        });
+
+        _.defaults(query.options, {
             sort: [],
             skip: 0,
             limit: 10
-        };
+        });
 
-        if ( callbacks.beforePublish && _.isFunction( callbacks.beforePublish ) )
-            query = callbacks.beforePublish( query, this ) || query;
+        if (callbacks.beforePublish) {
+            query = callbacks.beforePublish(query, this) || query;
+        }
 
-        //logger.log('query.selector\n', JSON.stringify(query.selector, null, 2) )
-        //logger.log('query.options\n', JSON.stringify(query.options, null, 2) )
 
-        var cursor = collection.find( query.selector, query.options );
+        var cursor = collection.find(query.selector, query.options);
 
-        if ( callbacks.afterPublish && _.isFunction( callbacks.afterPublish ) )
-            cursor = callbacks.afterPublish( 'results', cursor, this ) || cursor;
+        if (callbacks.afterPublish) {
+            cursor = callbacks.afterPublish('results', cursor, this) || cursor;
+        }
 
-        return cursor;
-    } );
+        FilterCollections._extendedPublishCursor(cursor, this, publisherResultsCollectionName, publisherResultsId);
+
+        // Call ready since the extended publish cursor, like the official publish cursor version, does not call
+        // ready by itself.
+        self.ready();
+    });
 
     /**
      * Publish result count.
      */
 
-    Meteor.publish( publisherCountId, function ( query ) {
-        check( query, Object );
+    Meteor.publish(publisherCountId, function (query) {
         var self = this;
         var allow = true;
         var cursor = {};
 
-        if ( callbacks.allow && _.isFunction( callbacks.allow ) )
-            allow = callbacks.allow( query, this );
-
-        if ( !allow ) {
-            throw new Meteor.Error( 417, 'Not allowed' );
+        if (callbacks.allow
+            && _.isFunction(callbacks.allow)) {
+            allow = callbacks.allow(query, this);
         }
 
-        query = (query && !_.isEmpty( query )) ? query : {};
-        query.selector = query.selector || {};
+        if (!allow) {
+            throw new Meteor.Error(417, 'Not allowed');
+        }
 
-        if ( callbacks.beforePublish && _.isFunction( callbacks.beforePublish ) )
-            query = callbacks.beforePublish( query, this ) || query;
+        query = query || {};
+        _.defaults(query, {
+            selector: {}
+        });
 
-        count = collection.find( query.selector ).count() || 0;
+        if (callbacks.beforePublish) {
+            query = callbacks.beforePublish(query, this) || query;
+        }
 
-        if ( callbacks.afterPublish && _.isFunction( callbacks.afterPublish ) )
-            cursor = callbacks.afterPublish( 'count', cursor, this ) || cursor;
+        var count = collection.find(query.selector).count() || 0;
 
-        self.added( publisherCountCollectionName, Meteor.uuid(), {
+        if (callbacks.afterPublish) {
+            cursor = callbacks.afterPublish('count', cursor, this) || cursor;
+        }
+
+        self.added(publisherCountCollectionName, Meteor.uuid(), {
             count: count,
             query: query
-        } );
+        });
 
-        this.ready();
-    } );
+        self.ready();
+    });
 };
